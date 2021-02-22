426958eeea46077dd0e9004ee7e6847f61bf7d48
nattaporn02 <75599794+nattaporn02@users.noreply.github.com>
2021-02-22 11:37:22 +0700

    Add files via upload

--- แบ่งGroup.c
+++ แบ่งGroup.c
@@ -2,16 +2,17 @@
 
 int main(void){
 	int i = 0;
+	int a;
 	int people;
 	printf("Enter score : ");
-	scanf("%d", & i);
+	scanf("%d", & a);
 	printf("Enter people : ");
 	scanf("%d", & people);
 	for(i = 0;i <= 10;i++){
-		if(i >= 70){
+		if(a >= 70){
 		printf("Group A");
 	}
-		if(i <= 70){
+		if(a <= 70){
 		printf("Group B");
 	}
 	return 0;
